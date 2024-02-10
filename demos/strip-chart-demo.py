db4d6170f4a29878aff7f2c854759cd6e4cf2ac0
R Evan McClellan <vanlellan@gmail.com>
2024-02-10 12:38:52 -0600

    add GPLv3 header to demo codes

--- demos/strip-chart-demo.py
+++ demos/strip-chart-demo.py
@@ -1,5 +1,22 @@
-#!/bin/python3
+#!/usr/bin/python3
+
 #display a live-updating strip chart
+#2024-02-10 Randall Evan McClellan
+
+#This file is part of PSC-seismometer.
+#
+#    PSC-seismometer is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    PSC-seismometer is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with PSC-seismometer.  If not, see <http://www.gnu.org/licenses/>.
 
 
 import math as m
