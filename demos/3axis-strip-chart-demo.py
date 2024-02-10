f20e30ab7e6bdf9c12dc72d026d4d6a158b66709
R Evan McClellan <vanlellan@gmail.com>
2024-02-10 12:31:22 -0600

    finish 3axis demo code

--- demos/3axis-strip-chart-demo.py
+++ demos/3axis-strip-chart-demo.py
@@ -10,43 +10,45 @@ from matplotlib.lines import Line2D
 import time
 
 class Strip:
-    def __init__(self, ax, ay, az):
+    def __init__(self, ax, ay, az, resolution=10, tmax=5):
         self.ax = ax
         self.ay = ay
         self.az = az
-        self.tdata = [0.0]*100
-        self.xdata = [0.0]*100
-        self.ydata = [0.0]*100
-        self.zdata = [0.0]*100
+        self.resolution = resolution
+        self.tmax = tmax
+        self.tdata = [0.0]*(tmax*1000//(resolution+10)) #there's an extra ~10ms delay of processing time at each iteration, I think
+        self.xdata = [0.0]*(tmax*1000//(resolution+10))
+        self.ydata = [0.0]*(tmax*1000//(resolution+10))
+        self.zdata = [0.0]*(tmax*1000//(resolution+10))
         self.xline = Line2D(self.tdata, self.xdata)
         self.yline = Line2D(self.tdata, self.ydata)
         self.zline = Line2D(self.tdata, self.zdata)
         self.ax.add_line(self.xline)
         self.ax.set_ylim(-1.1, 1.1)
-        self.ax.set_xlim(0, 10.0)
+        self.ax.set_xlim(0, self.tmax+1)
         self.ay.add_line(self.yline)
         self.ay.set_ylim(-1.1, 1.1)
-        self.ay.set_xlim(0, 10.0)
+        self.ay.set_xlim(0, self.tmax+1)
         self.az.add_line(self.zline)
         self.az.set_ylim(-1.1, 1.1)
-        self.az.set_xlim(0, 10.0)
+        self.az.set_xlim(0, self.tmax+1)
 
     #update is called every interval to update the data and graph
     def update(self, x):
         self.tdata.append(x[0]) #append new time value
         self.tdata.pop(0)       #remove oldest time value
-        self.ydata.append(x[1]) #append new ydata value
-        self.xdata.pop(0)       #remove oldest ydata value
-        self.xdata.append(x[1]) #append new ydata value
+        self.ydata.append(x[1]) #append new xdata value
+        self.xdata.pop(0)       #remove oldest xdata value
+        self.xdata.append(x[2]) #append new ydata value
         self.ydata.pop(0)       #remove oldest ydata value
-        self.zdata.append(x[1]) #append new ydata value
-        self.zdata.pop(0)       #remove oldest ydata value
+        self.zdata.append(x[3]) #append new zdata value
+        self.zdata.pop(0)       #remove oldest zdata value
         self.xline.set_data(self.tdata, self.xdata)
         self.yline.set_data(self.tdata, self.ydata)
         self.zline.set_data(self.tdata, self.zdata)
-        self.ax.set_xlim(self.tdata[0], self.tdata[0]+10.0) #shift the xlimits of the graph to match tdata range
-        self.ay.set_xlim(self.tdata[0], self.tdata[0]+10.0) #shift the xlimits of the graph to match tdata range
-        self.az.set_xlim(self.tdata[0], self.tdata[0]+10.0) #shift the xlimits of the graph to match tdata range
+        self.ax.set_xlim(self.tdata[0], self.tdata[0]+self.tmax+1) #shift the xlimits of the xgraph to match tdata range + 1s extra
+        self.ay.set_xlim(self.tdata[0], self.tdata[0]+self.tmax+1) #shift the xlimits of the ygraph to match tdata range + 1s extra
+        self.az.set_xlim(self.tdata[0], self.tdata[0]+self.tmax+1) #shift the xlimits of the zgraph to match tdata range + 1s extra
         return self.xline, self.yline, self.zline
 
 startTime = float(time.time())
@@ -60,10 +62,10 @@ def wave(st = startTime):
         yield t, aX, aY, aZ
 
 
-fig, axes = plt.subplots(3)
-scope = Strip(axes[0], axes[1], axes[2])
+fig, (ax, ay, az) = plt.subplots(3,sharex=True)
+scope = Strip(ax, ay, az)
 
-ani = animation.FuncAnimation(fig, scope.update, wave, interval=50, blit=True, save_count=100)
+ani = animation.FuncAnimation(fig, scope.update, wave, interval=scope.resolution, blit=True, save_count=5)
 
 plt.show()
 
