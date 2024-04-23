597cb2e5a3396ed55c472f5e26b8e611585817ef
PSC Seismometer Raspberry Pi <remcclellan@pensacolastate.edu>
2024-04-23 08:42:27 -0500

    local updates to readout.py

--- readout.py
+++ readout.py
@@ -2,7 +2,7 @@
 # -*- coding:utf-8 -*-
 
 
-import time
+import time as t
 import ADS1256
 import RPi.GPIO as GPIO
 
@@ -12,17 +12,21 @@ try:
     ADC.ADS1256_init()
 
     with open("seismo.txt","a") as data:
-
+        adcV = [0.0,0.0,0.0,0.0,0.0]
         while(1):
-            adc0 = ADC_Value[0]*5.0/0x7fffff
-            adc1 = ADC_Value[1]*5.0/0x7fffff
-            adc2 = ADC_Value[2]*5.0/0x7fffff
-            adc3 = ADC_Value[3]*5.0/0x7fffff
-            data.write(adc0,",", adc1,",", adc2,",", adc3,",","\n")
+            t.sleep(0.5)
             ADC_Value = ADC.ADS1256_GetAll()
-        print ("diff 0-1 = %lf V"%((ADC_Value[0]-ADC_Value[1])*5.0/0x7fffff))
-        print ("diff 2-3 = %lf V"%((ADC_Value[2]-ADC_Value[3])*5.0/0x7fffff))
-        print ("\33[3A")
+            adcV[0] = ADC_Value[0]*5.0/0x7fffff
+            adcV[1] = ADC_Value[1]*5.0/0x7fffff
+            adcV[2] = ADC_Value[2]*5.0/0x7fffff
+            adcV[3] = ADC_Value[3]*5.0/0x7fffff
+            adcV[4] = t.time()
+            print(adcV)
+            data.write(str(adcV)+'\n')
+            print ("time = %lf"%(adcV[4]))
+            print ("diff 0-1 = %lf V"%((ADC_Value[0]-ADC_Value[1])*5.0/0x7fffff))
+            print ("diff 2-3 = %lf V"%((ADC_Value[2]-ADC_Value[3])*5.0/0x7fffff))
+            print ("\33[5A")
 
         
 except :
