89456037ffb9439767fabc5af179ec96997be7fe
PSC Seismometer Raspberry Pi <remcclellan@pensacolastate.edu>
2024-04-26 12:22:47 -0500

    updates to readout.py

--- readout.py
+++ readout.py
@@ -16,17 +16,17 @@ try:
         while(1):
             t.sleep(0.5)
             ADC_Value = ADC.ADS1256_GetAll()
-            adcV[0] = ADC_Value[0]*5.0/0x7fffff
-            adcV[1] = ADC_Value[1]*5.0/0x7fffff
-            adcV[2] = ADC_Value[2]*5.0/0x7fffff
-            adcV[3] = ADC_Value[3]*5.0/0x7fffff
+            adcV[0] = ADC_Value[0]*5000.0/0x7fffff  #mV
+            adcV[1] = ADC_Value[1]*5000.0/0x7fffff  #mV
+            adcV[2] = ADC_Value[2]*5000.0/0x7fffff  #mV
+            adcV[3] = ADC_Value[3]*5000.0/0x7fffff  #mV
             adcV[4] = t.time()
-            print(adcV)
-            data.write(str(adcV)+'\n')
-            print ("time = %lf"%(adcV[4]))
-            print ("diff 0-1 = %lf V"%((ADC_Value[0]-ADC_Value[1])*5.0/0x7fffff))
-            print ("diff 2-3 = %lf V"%((ADC_Value[2]-ADC_Value[3])*5.0/0x7fffff))
-            print ("\33[5A")
+            #print(adcV)
+            data.write(f"{adcV[0]:.3f},{adcV[1]:.3f},{adcV[2]:.3f},{adcV[3]:.3f},{adcV[4]:.3f}\n")
+            print (f"time = {adcV[4]:.3f}")
+            print (f"diff 0-1 = {(ADC_Value[0]-ADC_Value[1])*5000.0/0x7fffff:.3f}")
+            print (f"diff 2-3 = {(ADC_Value[2]-ADC_Value[3])*5000.0/0x7fffff:.3f}")
+            print ("\33[4A")
 
         
 except :
