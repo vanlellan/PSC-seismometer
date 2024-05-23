ec7d77d42aa2b5b6d9b44a9d66d0ef225dd80b35
PSC Seismometer Raspberry Pi <remcclellan@pensacolastate.edu>
2024-05-23 11:20:43 -0500

    add diff measurement code

--- diff-read.py
+++ diff-read.py
@@ -5,14 +5,16 @@
 import time as t
 import ADS1256
 import RPi.GPIO as GPIO
-
+import traceback
 
 try:
     ADC = ADS1256.ADS1256()
     ADC.ADS1256_init()
+    ADC.ADS1256_SetMode(1)
 
-    with open("seismo.txt","a") as data:
+    with open("seismodiff.txt","a") as data:
         adcV = [0.0,0.0,0.0,0.0,0.0]
+        ADC_Value = ADC.ADS1256_GetAll()    #throw away first readout
         while(1):
          #   t.sleep(0.5)
             ADC_Value = ADC.ADS1256_GetAll()
@@ -24,17 +26,15 @@ try:
             #print(adcV)
             data.write(f"{adcV[0]:.3f},{adcV[1]:.3f},{adcV[2]:.3f},{adcV[3]:.3f},{adcV[4]:.3f}\n")
             print (f"time = {adcV[4]:.3f}")
-            #print (f"diff 0-1 = {(ADC_Value[0]-ADC_Value[1])*5000.0/0x7fffff:.3f}")
-            #print (f"diff 2-3 = {(ADC_Value[2]-ADC_Value[3])*5000.0/0x7fffff:.3f}")
-            #print ("\33[4A")
-            print (f"adc0 = {(ADC_Value[0])*5000.0/0x7fffff:.3f}")
-            print (f"adc1 = {(ADC_Value[1])*5000.0/0x7fffff:.3f}")
-            print (f"adc2 = {(ADC_Value[2])*5000.0/0x7fffff:.3f}")
-            print (f"adc3 = {(ADC_Value[3])*5000.0/0x7fffff:.3f}")
+            print (f"diffadc0 = {(ADC_Value[0])*5000.0/0x7fffff:.3f}")
+            print (f"diffadc1 = {(ADC_Value[1])*5000.0/0x7fffff:.3f}")
+            print (f"diffadc2 = {(ADC_Value[2])*5000.0/0x7fffff:.3f}")
+            print (f"diffadc3 = {(ADC_Value[3])*5000.0/0x7fffff:.3f}")
             print ("\33[6A")
 
         
-except :
+except Exception:
+    traceback.print_exc()
     GPIO.cleanup()
     print ("\r\nProgram end     ")
     exit()
--- plotdiff.py
+++ plotdiff.py
@@ -9,8 +9,8 @@ ch2 = []
 ch3 = []
 cht = []
 
-with open("seismo.txt","r") as dfile:
-    for i in range(500):
+with open("seismodiff.txt","r") as dfile:
+    for i in range(350):
         line = dfile.readline()
         if True:
             data = line.split(',')
@@ -20,14 +20,12 @@ with open("seismo.txt","r") as dfile:
             ch3.append(float(data[3]))
             cht.append(float(data[4]))
 
-ch01 = [x-y for x,y in zip(ch0,ch1)]
-ch23 = [x-y for x,y in zip(ch2,ch3)]
 
 plt.plot(ch0)
 plt.show()
 plt.plot(ch1)
 plt.show()
-plt.plot(ch01)
+plt.plot(ch2)
 plt.show()
-plt.plot(ch23)
+plt.plot(ch3)
 plt.show()
